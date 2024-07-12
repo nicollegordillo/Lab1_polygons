8c7abca5ec6d6244fa70483aaeaa9d5b8cd62963
nicollegordillo <nicollegordillo912@gmail.com>
2024-07-12 00:56:39 -0600

    Feat: Added polygon 4 and 5

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,34 @@ fn main() {
         Vec3::new(436.0, 249.0, 0.0),
     ];
 
+    let polygon4_vertices = vec![
+        Vec3::new(413.0, 177.0, 0.0),
+        Vec3::new(448.0, 159.0, 0.0),
+        Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0),
+        Vec3::new(535.0, 36.0, 0.0),
+        Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0),
+        Vec3::new(750.0, 145.0, 0.0),
+        Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0),
+        Vec3::new(659.0, 214.0, 0.0),
+        Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0),
+        Vec3::new(580.0, 230.0, 0.0),
+        Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0),
+        Vec3::new(517.0, 144.0, 0.0),
+        Vec3::new(466.0, 180.0, 0.0),
+    ];
+
+    let polygon5_vertices = vec![
+        Vec3::new(682.0, 175.0, 0.0),
+        Vec3::new(708.0, 120.0, 0.0),
+        Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0),
+    ];
+
     framebuffer.set_current_color(0xFFC857);
     framebuffer.fill_polygon(&polygon1_vertices, 0xFFC857);
 
@@ -52,13 +80,20 @@ fn main() {
 
     framebuffer.set_current_color(0xD62246);
     framebuffer.fill_polygon(&polygon3_vertices, 0xD62246);
+
+    framebuffer.set_current_color(0x23CE6B);
+    framebuffer.fill_polygon(&polygon4_vertices, 0x23CE6B);
+
+    framebuffer.set_current_color(0xFF000000);
+    framebuffer.fill_polygon(&polygon5_vertices, 0xFF000000);
     
     // Draw each polygon with its respective colors
     framebuffer.set_current_color(0xffffff);
     draw_polygon(&mut framebuffer, &polygon1_vertices); 
     draw_polygon(&mut framebuffer, &polygon2_vertices);
     draw_polygon(&mut framebuffer, &polygon3_vertices);
-    
+    draw_polygon(&mut framebuffer, &polygon4_vertices); 
+    draw_polygon(&mut framebuffer, &polygon5_vertices);
 
     // Flip the image vertically (BMP format requirement)
     framebuffer.flip_vertical();
